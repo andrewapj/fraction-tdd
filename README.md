54a09403eb8e7894e381a0bd260356a0f9946915
Andrew Johnson <andrew.apj@gmail.com>
2018-11-30 21:30:37 +0000

    Initial commit including the following: - Failing test - git ignore file - LICENSE - Maven Pom file - Read Me file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fraction TDD
+
+https://online-training.jbrains.ca/courses/wbitdd-01/lectures/133270
+
+An implementation of the Fraction TDD exercise from JBrains.ca
---
+++ src/test/java/com/github/andrewapj/FractionTest.java
@@ -0,0 +1,12 @@
+package com.github.andrewapj;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FractionTest {
+
+    @Test
+    void test(){
+        Assertions.fail();
+    }
+}
